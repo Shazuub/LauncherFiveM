ee44de5e45e59d6458d81f7abe05f18cb0eb7498
Shazuub <54162106+Shazuub@users.noreply.github.com>
2021-06-26 17:55:00 +0200

    Initial Commit

---
+++ LauncherFiveM/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Launcher))
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Location = New System.Drawing.Point(64, 388)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(220, 50)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Jouer"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(290, 388)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(220, 50)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "TeamSpeak"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.SystemColors.AppWorkspace
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(516, 388)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(220, 50)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Github"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Launcher"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FiveM Launcher"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ LauncherFiveM/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Launcher
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Shell("Explorer.exe fivem://connect/127.0.0.1:30120")
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Shell("Explorer.exe ts3server://ts.XXXXXXXXX.com")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Shell("Explorer.exe https://github.com/Shazuub/LauncherFiveM")
+    End Sub
+End Class
